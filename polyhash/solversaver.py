14a1cbeb9ba8abd1840f290ec4f7b2dbdf80f5f2
Simon Bessenay <simon.bessenay@etu.univ-nantes.fr>
2017-11-14 11:23:22 +0100

    Modification classe SolverSaver

--- polyhash/solversaver.py
+++ polyhash/solversaver.py
@@ -6,4 +6,17 @@ __all__ = ['SolverSaver'] # ajouter dans cette liste tous les symboles 'importab
 class SolverSaver:
     """
     Classe ayant pour but de sauver les résultats dans le fichier solution
+    - fileName -> nom du fichier de solution
+    - placedRouter -> liste des routeurs placés
     """
+
+    def __init__(self, fileName, placedRouter):
+        """Constructeur de la classe"""
+        self.fileName = fileName
+        self.placedRouter = placedRouter
+
+    def writeInFile(self):
+        """Fonction qui écrit la solution dans le fichier"""
+        file = open(self.fileName, "w")
+        
+        file.close()

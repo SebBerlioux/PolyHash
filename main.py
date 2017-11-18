0f4f80a2448a47b88ded599ef4137a9933a8a891
Nicolas Cuadros <nicolas.cuadirs@etu.univ-nantes.fr>
2017-11-18 03:35:33 -0800

    Amélioration des chemines, et calcul du temps d'analyse et de placement

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 
 from polyhash import *
 def main():
-    #récupère la carte
+    CLOCK = PersonnalClock()
+    print("............LECTURE DU FICHIER............")
     mapIn = Map(getArgument()[1])
+    print("............LECTURE FINIT............")
+    
+    print("............DEBUT DE L'ANALYSE............")
+    CLOCK.begin()
+    mapIn.analyseMap()
+    timeELapsed = CLOCK.getElapsedTime()
+    CLOCK.end()
+    print("DUREE DE L'ANALYSE : ",str(timeELapsed)," SECONDES")
 
-    test2 = Path(mapIn.map[20][20], mapIn.map[25][20], 2)
+
+    print("............DEBUT DU PLACEMENT............")
+    CLOCK.begin()
+    mapIn.placeRouter()
+    timeELapsed = CLOCK.getElapsedTime()
+    CLOCK.end()
+    print("DUREE DU PLACEMENT : ",str(timeELapsed)," SECONDES")
+    for router in mapIn.placedRouter:
+        for case in router.backRoad.fiberCase:
+            mapIn.asciiMap[case[1]][case[0]] = 'E'
+        mapIn.asciiMap[router.row][router.column] = 'B'
+    """
+    coordA = (150,100)
+    coordB = (25,100)
+    test2 = Path(mapIn.map[coordA[0]][coordA[1]], mapIn.map[coordB[0]][coordB[1]], 2)
     print(test2.fiberCase)
     for case in test2.fiberCase:
         mapIn.asciiMap[case[1]][case[0]] = 'E'
-    #mapIn.analyseMap()
+    mapIn.asciiMap[coordA[0]][coordA[1]] = 'B'
+    mapIn.asciiMap[coordB[0]][coordB[1]] = 'B'
+    """
+    print("SAUVEGARDE DE LA CARTE EN IMAGE")
     mapIn.saveASCIIMap()
 
 if __name__ == '__main__':
